9603e6bfda7f0b92878935dff250e1e392f0a1f7
TopGitSan <c.toparcean@gmail.com>
2024-10-30 18:12:40 +0100

    feat(posts-lists): :sparkles: show and style the list of posts

--- src/app/features/posts/posts-list/posts-list.component.ts
+++ src/app/features/posts/posts-list/posts-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { PostsService } from '../../../graphQL/services/posts.service';
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './posts-list.component.html',
-  styleUrl: './posts-list.component.scss'
+  styleUrl: './posts-list.component.scss',
 })
 export class PostsListComponent {
+  private readonly postsService = inject(PostsService);
 
+  posts$ = this.postsService.queryPosts();
 }
