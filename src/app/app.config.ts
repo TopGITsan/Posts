aa9b806763de869cb8d1efdc8f76a298fdb619b7
TopGitSan <c.toparcean@gmail.com>
2024-10-30 17:30:12 +0100

    chore(ngRx): :heavy_plus_sign: add ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
   inject,
+  isDevMode,
+  provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+import { routes } from './app.routes';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,13 +22,22 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideApollo(() => {
       const httpLink = inject(HttpLink);
-
       return {
         link: httpLink.create({
-          uri: 'https://graphqlzero.almansi.me/api',
+          uri: environment.graphQlApi,
         }),
         cache: new InMemoryCache(),
       };
     }),
+    provideStore(
+      {},
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+        },
+      }
+    ),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
 };
