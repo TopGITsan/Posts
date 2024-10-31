9bea58649630b248a703d8aace757a9dcdb6c642
TopGitSan <c.toparcean@gmail.com>
2024-10-31 17:46:52 +0100

    feat(posts.reducer): :sparkles: on save post action store the post localy

--- src/app/features/posts/posts-store/posts/posts.effects.ts
+++ src/app/features/posts/posts-store/posts/posts.effects.ts
@@ -5,7 +5,6 @@ import {
   catchError,
   EMPTY,
   exhaustMap,
-  filter,
   map,
   of,
   switchMap,
--- src/app/features/posts/posts-store/posts/posts.reducer.ts
+++ src/app/features/posts/posts-store/posts/posts.reducer.ts
@@ -3,6 +3,7 @@ import {
   LOAD_POSTS,
   LOAD_POSTS_FAILURE,
   LOAD_POSTS_SUCCESS,
+  SAVE_POST,
   SELECT_POST_ID,
   UNSELECT_POST_ID,
 } from './posts.actions';
@@ -29,7 +30,23 @@ const createPostsReducer = createReducer(
   on(SELECT_POST_ID, UNSELECT_POST_ID, (state, { postId }) => ({
     ...state,
     selectedPostId: postId,
-  }))
+  })),
+  on(SAVE_POST, (state, { post }) => {
+    const postsCount = state.posts.length;
+
+    return {
+      ...state,
+      loading: false,
+      posts: [
+        {
+          ...post,
+          id: String(postsCount + 1),
+          user: { ...post.user, id: String(postsCount + 1) },
+        },
+        ...state.posts,
+      ],
+    };
+  })
 );
 
 export function postsReducer(state: PostsState | undefined, action: Action) {
