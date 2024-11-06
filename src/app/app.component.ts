51d35958f24f1242859dd048e03a85526b5195e8
TopGitSan <c.toparcean@gmail.com>
2024-11-06 11:56:49 +0100

    refactor(header): :recycle: extract header into its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { StoreFacadeService } from './store/store-facade.service';
 import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, AsyncPipe],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    AsyncPipe,
+    HeaderComponent,
+  ],
   selector: 'app-root',
   standalone: true,
   styleUrl: './app.component.scss',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  #storeFacade = inject(StoreFacadeService);
-  title$ = this.#storeFacade.getHeaderTitle();
-}
+export class AppComponent {}
