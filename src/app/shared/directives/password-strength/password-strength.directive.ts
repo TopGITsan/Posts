eb08f593097ec1eedeac9be5461b1ae7583117cf
TopGitSan <c.toparcean@gmail.com>
2024-11-07 17:02:23 +0100

    feat(password-strength.directive): :sparkles: evaluator function can be provided as an input

--- src/app/shared/directives/password-strength/password-strength.directive.ts
+++ src/app/shared/directives/password-strength/password-strength.directive.ts
@@ -1,5 +1,8 @@
 import { Directive, inject, input } from '@angular/core';
-import { PasswordStrength } from './password-strenght.type';
+import {
+  PasswordEvaluatorFn,
+  PasswordStrength,
+} from './password-strenght.type';
 import { EVALUATOR_FN_TOKEN } from './password-strength.token';
 import { defaultEvaluatorFn } from './default-password-evaluator';
 
@@ -12,31 +15,12 @@ import { defaultEvaluatorFn } from './default-password-evaluator';
   },
 })
 export class PasswordStrengthDirective {
-  noStrengthCheck = input<boolean>(false);
-  // private readonly el = inject(ElementRef);
   strength: PasswordStrength = 'no';
-  // no need for ElementRef anymore
+  noStrengthCheck = input<boolean>(false);
+  evaluatorFn = input<PasswordEvaluatorFn>(
+    inject(EVALUATOR_FN_TOKEN, { optional: true }) ?? defaultEvaluatorFn
+  );
 
-  evaluatorFn =
-    inject(EVALUATOR_FN_TOKEN, { optional: true }) ?? defaultEvaluatorFn;
-
-  // add or remove css classes based on evaluated password
-  // onInput(event: InputEvent): void {
-  //   if (this.noStrengthCheck()) {
-  //     return;
-  //   }
-  //   const input = event.target as HTMLInputElement;
-  //   const value = input.value;
-  //   const strength = this.evaluatePasswordStrength(value);
-  //   console.log(' input event : ', { event, strength });
-  //   for (let i = this.el.nativeElement.classList.length - 1; i >= 0; i--) {
-  //     const className = this.el.nativeElement.classList[i];
-  //     if (className.startsWith('password-strength-')) {
-  //       this.el.nativeElement.classList.remove(className);
-  //     }
-  //   }
-  //   this.el.nativeElement.classList.add(`password-strength-${strength}`);
-  // }
   onInput(event: InputEvent): void {
     if (this.noStrengthCheck()) {
       return;
@@ -44,7 +28,25 @@ export class PasswordStrengthDirective {
     const input = event.target as HTMLInputElement;
     const value = input.value;
 
-    this.strength = this.evaluatorFn(value);
-    console.log(' input event : ', { event, strength: this.strength });
+    this.strength = this.evaluatorFn()(value);
+    // console.log(' input event : ', { event, strength: this.strength });
   }
 }
+
+// add or remove css classes based on evaluated password
+// onInput(event: InputEvent): void {
+//   if (this.noStrengthCheck()) {
+//     return;
+//   }
+//   const input = event.target as HTMLInputElement;
+//   const value = input.value;
+//   const strength = this.evaluatePasswordStrength(value);
+//   console.log(' input event : ', { event, strength });
+//   for (let i = this.el.nativeElement.classList.length - 1; i >= 0; i--) {
+//     const className = this.el.nativeElement.classList[i];
+//     if (className.startsWith('password-strength-')) {
+//       this.el.nativeElement.classList.remove(className);
+//     }
+//   }
+//   this.el.nativeElement.classList.add(`password-strength-${strength}`);
+// }
